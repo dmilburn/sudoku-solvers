a5fb1005b17e175964134aebd08f4d9bb3fc892f
Debbie Milburn <debbiemilburn2@gmail.com>
2017-03-10 19:30:03 -0500

    Cells know about their box

--- ruby/cell.rb
+++ ruby/cell.rb
@@ -6,7 +6,13 @@ class Cell
     @value = args[:value]
     @column = args[:column]
     @row = args[:row]
+    @box = decide_box
   end
+
+  def decide_box
+    (@row/3 * 2) + @row/3 + @column/3
+  end
+
 
 end
 
